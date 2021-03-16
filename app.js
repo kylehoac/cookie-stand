36f01a6b2826bf16c4873c17d836ad44a3de624f
Kyle Hoac <hoackyle@gmail.com>
2021-03-15 18:39:25 -0700

    random numbers and avg sales apply to list elements

--- app.js
+++ app.js
@@ -1,53 +1,43 @@
 const seattle = {
     name: 'Seattle',
-    hourlySales: [16, 20, 35, 48, 56, 77, 93, 144, 119, 84, 61, 23, 42, 57],
-    getRandomInt: function(min,max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min);
-    }
+    hourlySales: fillArray(23, 63, 6.3),
+    min: 23,
+    max: 63,
+    avgSale: 6.3,
 }
-seattle.getRandomInt(23,65);
+
 const tokyo = {
     name: 'Tokyo',
-    hourlySales: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-    getRandomInt: function(min,max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min);
-    }
+    hourlySales: fillArray(3, 24, 1.2),
+    min: 3,
+    max: 24,
+    avgSale: 1.2,
 }
-tokyo.getRandomInt(3,24)
+
 const dubai = {
     name: 'Dubai',
-    hourlySales: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-    getRandomInt: function(min,max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min);
-    }
+    hourlySales: fillArray(11, 38, 3.7),
+    min: 11,
+    max: 38,
+    avgSale: 3.7,
 }
-dubai.getRandomInt(11,38)
+
 const paris = {
     name: 'Paris',
-    hourlySales: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-    getRandomInt: function(min,max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min);
-    }
+    hourlySales: fillArray(20, 38, 2.3),
+    min: 20,
+    max: 38,
+    avgSale: 2.3,
 }
-paris.getRandomInt(20,38)
+
 const lima = {
     name: 'Lima',
-    hourlySales: [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-    getRandomInt: function(min,max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min);
-    }
+    hourlySales: fillArray(2, 16, 4.6),
+    min: 2,
+    max: 16,
+    avgSale: 4.6,
 }
-lima.getRandomInt(2,16)
+
 const timeSlots = [
     '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',
 ]
@@ -97,3 +87,25 @@ function renderCookieStand(cookieStand) {
     ulElem.appendChild(totalItemElem);
     totalItemElem.textContent = 'Total ' + total + ' cookies';
 }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function fillArray(min, max, average) {
+    values = []
+    for (let i = 0; i < 14; i += 1) {
+
+        let number = getRandomInt(min, max)
+        number = number * average
+       
+        console.log(number)
+        values.push(Math.round(number))
+    }
+    return values
+}
+
+
+
