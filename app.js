7a4f7f882c100bb5da082bb02c608bf0bd57dd27
Kyle Hoac <hoackyle@gmail.com>
2021-03-18 13:52:47 -0700

    finished bottom row

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const timeSlots = [
     '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'
 ]
 
-let CookieStands = function (name, min, max, avgSale,) {
+let CookieStands = function (name, min, max, avgSale) {
     this.name = name;
     this.min = min;
     this.max = max;
     this.avgSale = avgSale;
     this.hourlySales = [];
     for (let i = 0; i < timeSlots.length - 1; i++) {
-        this.hourlySales.push(this.generateRandom(this.min, this.max));
+        this.hourlySales.push(Math.floor(this.avgSale * this.generateRandom(this.min, this.max)));
     }
     this.calculateTotal();
 }
@@ -30,15 +30,6 @@ let tokyo = new CookieStands('Tokyo', 3, 24, 1.2);
 let dubai = new CookieStands('Dubai', 11, 38, 3.74);
 let paris = new CookieStands('Paris', 20, 38, 2.3);
 let lima = new CookieStands('Lima', 2, 16, 4.6);
-// let total = new CookieStands('Total',[])
-
-// CookieStands.prototype.GrandTotal = function () {
-//     let gTotal = []
-//     for (let i = 0; i < timeSlots.length - 1; i++) {
-//         gTotal = + seattle[0] + tokyo[0] + dubai[0] + paris[0] + lima[0]
-//     }
-// }
-
 
 const storeContainerElem = document.getElementById('storeContainer');
 
@@ -71,8 +62,8 @@ CookieStands.prototype.render = function () {
     for (i = 0; i < this.hourlySales.length; i++) {
         const tdVal = document.createElement('td');
         trElem.appendChild(tdVal);
-        console.log(this.hourlySales)
-        tdVal.textContent = Math.round(this.hourlySales[i] * this.avgSale);
+        // console.log(this.hourlySales)
+        tdVal.textContent = this.hourlySales[i];
     }
 
 }
@@ -81,18 +72,44 @@ tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
+grandTotal();
 
+function grandTotal() {
+    let totalHourlySales = []
+    // totalHourlySales.push(hourlySales)
+    let sum = 0;
+    const totalElem = document.createElement('tr');
+    tableElem.appendChild(totalElem);
 
+    const totalTextElem = document.createElement('td')
+    totalTextElem.textContent = "Total "
+    totalElem.appendChild(totalTextElem);
+    for (let i = 0; i < timeSlots.length; i++) {
+        const sumTotal = document.createElement('td');
+        let columnSum = seattle.hourlySales[i] + tokyo.hourlySales[i] + dubai.hourlySales[i] + paris.hourlySales[i] + lima.hourlySales[i];
+        console.log(seattle.hourlySales[i], tokyo.hourlySales[i]);
+        sumTotal.textContent = columnSum;
+        totalElem.appendChild(sumTotal);
+        sum += columnSum;
 
+        // for (let j = 0; j < this.hourlySales.length; j++) {
+        //     console.log(totalHourlySales[j][i])
+        //     // sum = sum + totalHourlySales[j][i];
+        // }
+    }
+    // const finalSum = document.createElement('td')
+    // finalSum.textContent = sum;
+    // totalElem.appendChild(finalSum);
+}
 // //make an array to hold all hoursly sales array
 // let testArray = []
 // //push all hourly sales array into this array
 // testArray.push(hourlySales)
 
 // testArray = [[1, 2, 3, 4, 5, 6, 7, 8],
-// [1, 2, 3, 4, 5, 6, 7, 8],
-// [1, 2, 3, 4, 5, 6, 7, 8],
-// [1, 2, 3, 4, 5, 6, 7, 8]]
+//              [1, 2, 3, 4, 5, 6, 7, 8],
+//              [1, 2, 3, 4, 5, 6, 7, 8],
+//              [1, 2, 3, 4, 5, 6, 7, 8]]
 // let sum = 0
 
 // //since all hourly sales arrays are qual length you can calculate the sum by teh way you travrse your nested arrray/for loop
@@ -104,15 +121,25 @@ lima.render();
 //     }
 // }
 
-CookieStands.prototype.GrandTotal = function () {
-    let gTotal = []
-    gTotal.push(hourlySales)
-    // gTotal = [[seattle, tokyo, dubai, paris, lima]]
-    let sum = 0
-    for (let i = 0; i < timeSlots.length; i++) {
-        for (let j = 0; j < timeSlots.length; j++) {
-            console.log(gTotal[j][i])
-            sum = sum + gTotal[j][i];
-        }
-    }
-}
+// CookieStands.prototype.GrandTotal = function () {
+//         let totalHourlySales = []
+//         // totalHourlySales.push(hourlySales)
+//         const len = CookieStands[0].hourlySales.length
+//     for (let i = 0; i < len; i++) {
+
+//         const dailySales = calculateSalesForAllCookieStandsForGivenHour(CookieStands, i)
+
+//     }
+// }
+// function calculateSalesForAllCookieStandsForGivenHour(CookieStands,i){
+//     let salesPerHour = 0;
+
+//     for (let j = 0; j < CookieStands[0]; j++) {
+//         const currentCookieStand = CookieStands[j];
+
+//         const hourlyTotal = currentCookieStand.hourlySales[i];
+
+//         salesPerHour += hourlyTotal
+//     }
+//         return salesPerHour;
+// }
