b38024985a1bb710de1457b817ee51d805687bce
Kyle Hoac <hoackyle@gmail.com>
2021-03-17 19:04:59 -0700

    added css to main page, cleaned up css on sales page

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
 const timeSlots = [
-    '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm','Total'
+    '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total'
 ]
 
 let CookieStands = function (name, min, max, avgSale,) {
@@ -9,15 +8,15 @@ let CookieStands = function (name, min, max, avgSale,) {
     this.max = max;
     this.avgSale = avgSale;
     this.hourlySales = [];
-    for (let i = 0; i < timeSlots.length-1; i++) {
+    for (let i = 0; i < timeSlots.length - 1; i++) {
         this.hourlySales.push(this.generateRandom(this.min, this.max));
     }
     this.calculateTotal();
 }
 
-CookieStands.prototype.calculateTotal = function(){
+CookieStands.prototype.calculateTotal = function () {
     let total = 0
-    for (let j = 0; j < timeSlots.length-1; j++){
+    for (let j = 0; j < timeSlots.length - 1; j++) {
         total = total + this.hourlySales[j]
     }
     this.hourlySales.push(total)
@@ -31,6 +30,14 @@ let tokyo = new CookieStands('Tokyo', 3, 24, 1.2);
 let dubai = new CookieStands('Dubai', 11, 38, 3.74);
 let paris = new CookieStands('Paris', 20, 38, 2.3);
 let lima = new CookieStands('Lima', 2, 16, 4.6);
+// let total = new CookieStands('Total',[])
+
+CookieStands.prototype.GrandTotal = function() {
+    let gTotal = []
+    for (let i = 0; i < timeSlots.length - 1; i++) {
+        gTotal =  + seattle[0] + tokyo[0] + dubai[0] + paris[0] + lima [0]
+    }
+}
 
 
 const storeContainerElem = document.getElementById('storeContainer');
@@ -67,12 +74,10 @@ CookieStands.prototype.render = function () {
         console.log(this.hourlySales)
         tdVal.textContent = Math.round(this.hourlySales[i] * this.avgSale);
     }
-    
-    }
 
-
-    seattle.render();
-    tokyo.render();
-    dubai.render();
-    paris.render();
-    lima.render();
+}
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
